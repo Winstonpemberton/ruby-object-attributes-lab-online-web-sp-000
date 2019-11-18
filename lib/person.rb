1323f2ad821a8eed031c42c3ce0c988db831cc1b
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-18 22:51:54 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,17 @@
-def name
-  @name
-end
+class Person
+  def name
+    @name
+  end
 
-def name=(new_name)
-  @name = new_name
-end
+  def name=(new_name)
+    @name = new_name
+  end
 
-def job
-  @job
-end
+  def job
+    @job
+  end
 
-def job=(new_job)
-  @job = new_job
+  def job=(new_job)
+    @job = new_job
+  end
 end
